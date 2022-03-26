88cf7607bb947061feda582aa2c64edb5ab60f1d
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-03-26 11:28:19 +0600

    final commit

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './Cart.css'
+ import './Cart.css'
 
-const Cart = () => {
+
+
+const Cart = ({cart}) => {
+
+    const clearAllItem = () =>{
+        
+    }
+   
     return (
         <div>
-            
+            <h3>order summary</h3>
+            {
+        cart.map(item => <p key={item.id}> {item.name}</p>)
+            }
+            <div>
+                <button className='Chose-btn'>Choose one</button> <br />
+                <button className='Clear-btn' onClick={()=>clearAllItem()}>New cart</button>
+            </div>
         </div>
     );
 };
--- src/Components/Component/Component.js
+++ src/Components/Component/Component.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import ProductDetails from '../prodDetails/ProductDetails';
+// import {chosehandle} from '../Cart/Cart'
+import Cart from '../Cart/Cart'
 import './Component.css'
 
 const Component = () => {
     const [products,setProducts] = useState([]);
     const [cart,setCart] = useState([])
+    
     useEffect(() => {
         fetch('data.json').then(res =>res.json()).then(data =>setProducts(data));
         },[])
 
 
         const orderHandleClick = (product) =>{
-            console.log(product);
             const newCart = [...cart,product];
             setCart(newCart);
      }
+    //  const chosehandle = ()=>{
+        //   setCart([])
+    //  }
         
     return (
         <div className='book-container'>
@@ -27,8 +32,7 @@ const Component = () => {
 
             </div>
             <div className="order-container">
-                <h3>order summary</h3>
-                <p>Added cart: {cart.length}</p>
+               <Cart cart={cart}></Cart>
             </div>
         </div>
     );
